b56cae1835f91ebbaa4bf69769d2b77562a1aa1e
Ryo <senko12365@icloud.com>
2025-05-16 09:42:02 +0300

    Update bot.go

--- bot.go
+++ bot.go
@@ -1,36 +1,41 @@
+
 package main
 
 import (
-	"log"
-	"os/exec"
-	"time"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("7747181762:AAEfarc6qwn455UFadykr-MP2jmnnbpu_Wc")
+	// Initialize bot with your token
+	bot, err := tgbotapi.NewBotAPI("7840497465:AAHfEoBzL1cGf1EyIhllm6SNbK5JUxS6a38")
 	if err != nil {
 		log.Panic(err)
 	}
 
 	bot.Debug = true
-	log.Printf("Бот запущен: @%s", bot.Self.UserName)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	chatID := int64(962449094)
-	for {
-		// Запускаем проверку
-		cmd := exec.Command("/bin/bash", "./main.sh")
-		output, err := cmd.CombinedOutput()
-		if err != nil {
-			log.Printf("Ошибка выполнения скрипта: %v", err)
-			continue
+	// Set up update configuration
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	// Get updates channel
+	updates := bot.GetUpdatesChan(u)
+
+	// Process incoming updates
+	for update := range updates {
+		if update.Message == nil {
+			continue // ignore any non-Message updates
 		}
 
-		msg := tgbotapi.NewMessage(chatID, string(output))
-		bot.Send(msg)
+		// Prepare a response message
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello, I received your message!")
+		msg.ReplyToMessageID = update.Message.MessageID
 
-		time.Sleep(5 * time.Second)
+		// Send the message
+		if _, err := bot.Send(msg); err != nil {
+			log.Printf("Error sending message: %v", err)
+		}
 	}
-
 }
